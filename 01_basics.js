88995234d0b795ebbdb11933e506698890922830
Vineet Daga <101184052+VineetDaga@users.noreply.github.com>
2023-10-09 07:53:12 +0000

    9/10/23

---
+++ 01_basics.js
@@ -0,0 +1,23 @@
+console.log("My Name is Vineet Daga ");
+
+const mobile = 9136129176;
+let f_name = "Vineet";
+var l_name = "Daga";
+age =21; // not the right way to declare a variable
+
+//  var is not used that much 
+//  because of issue in block scope and functional scope
+
+let height;
+// height is undefined
+
+console.log(mobile);
+console.log(f_name);
+console.log(l_name);
+console.log(age);
+
+//mobile = 9818194204 
+//this cannot be done because once const is defined and value is assigned it cannot be changed
+
+console.table([f_name,l_name,mobile,age,height]);
+
