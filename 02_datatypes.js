116c7d94ddbaba82ff566c916da6e823500efe02
Vineet Daga <101184052+VineetDaga@users.noreply.github.com>
2023-10-09 08:26:25 +0000

    9/10

---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"  //treat all js code as newer version 
+
+// alert(3+30)  not used in node js it is used for the browser 
+
+// Data types 
+let a = "vineet" //string
+let b = 599  //number
+let c = true  //booleans
+let d = null //null datatype
+let e; //undefined
+
+/* there are 7 primitive datatypes 
+1. number => ranges from 2^53
+2. strings 
+3. Booleans => true/false
+4. NUll => standalone value =>representation of empty value
+5. undefined => when value is not defined 
+6. Symbol => for uniqueness
+7. bigint => used for valuse greater than the range of numbers
+
+->object =>secondary datatype 
+*/
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+// the typeof null is object not null 
